db92caf12679f47965063e8b5dc167d004f7ffc0
liulx1127@163.com <liulx1127@163.com>
2022-08-06 23:24:41 +0800

    增加属性name、age， 和方法eat

--- TestModule01/src/com/test/test01.java
+++ TestModule01/src/com/test/test01.java
@@ -4,4 +4,11 @@ public class test01 {
     public static void main(String[] args){
         System.out.println("Hello Idea");
     }
+    private String name;
+    private int age;
+
+    public static void eat(String name){
+        name = this.name;
+
+    }
 }
