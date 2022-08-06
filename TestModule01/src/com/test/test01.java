b2d9d6d381170f35f8d9d8e0e6ef5e9683051536
liulx1127@163.com <liulx1127@163.com>
2022-08-07 00:07:32 +0800

    修改eat方法，及增加eat()调用

--- TestModule01/src/com/test/test01.java
+++ TestModule01/src/com/test/test01.java
@@ -3,12 +3,13 @@ package com.test;
 public class test01 {
     public static void main(String[] args){
         System.out.println("Hello Idea");
+        eat("Tester");
     }
     private String name;
     private int age;
 
-    public static void eat(String name){
-        name = this.name;
 
+    public static void eat(String name){
+        System.out.println(name + " eat");
     }
 }
